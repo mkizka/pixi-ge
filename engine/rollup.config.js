493d15caf291ed76b3b4ab28bb5e4ca025277f2a
mkizka <30231179+mkizka@users.noreply.github.com>
2020-07-07 01:04:36 +0900

    rollupへの移行

---
+++ engine/rollup.config.js
@@ -0,0 +1,24 @@
+import babel from '@rollup/plugin-babel'
+import typescript from '@rollup/plugin-typescript'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+    sourcemap: true
+  },
+  external: [...Object.keys(pkg.devDependencies || {})],
+  plugins: [
+    babel({
+      extensions: ['.js', '.ts']
+    }),
+    typescript({
+      declaration: true,
+      rootDir: 'src',
+      declarationDir: 'dist'
+    })
+  ]
+}
